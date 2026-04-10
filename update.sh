4712701ee3c8aeeb179472bb891c1d550401d5cc
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2026-04-10 21:10:56 +0200

    clean js files

--- update.sh
+++ update.sh
@@ -11,10 +11,19 @@ ProcessVPK
 ProcessToolAssetInfo
 FixUCS2
 
-# Prettify js/css
-rm --recursive resources/webinterface/dashboard_prettier/ || true
+# Prettify & Clean js/css
+rm --recursive --force resources/webinterface/dashboard_prettier/ resources/webinterface/dashboard_cleaned/ || true
 rsync --archive --include="*/" --include="*.js" --include="*.css" --exclude="*" --prune-empty-dirs resources/webinterface/dashboard/ resources/webinterface/dashboard_prettier/
-npm run prettier -- --write resources/webinterface/dashboard_prettier/ || true
+find resources/webinterface/dashboard_prettier/ -type f -name "*.js" -exec bash -c 'node .github/tools/generate_clean_js.mjs "$1" "${1/dashboard_prettier/dashboard_cleaned}"' _ {}  \; 
+npm run prettier -- --write resources/webinterface/dashboard_prettier/ resources/webinterface/dashboard_cleaned/  || true
+
+
+rm --recursive --force drivers/lighthouse/resources/webinterface_prettier/ drivers/lighthouse/resources/webinterface_cleaned/ || true
+rsync --archive --include="*/" --include="*.js" --include="*.css" --exclude="*" --prune-empty-dirs drivers/lighthouse/resources/webinterface drivers/lighthouse/resources/webinterface_prettier/
+find drivers/lighthouse/resources/webinterface_prettier/ -type f -name "*.js" -exec bash -c 'node .github/tools/generate_clean_js.mjs "$1" "${1/webinterface_prettier/webinterface_cleaned}"' _ {}  \; 
+npm run prettier -- --write drivers/lighthouse/resources/webinterface_prettier/ drivers/lighthouse/resources/webinterface_cleaned/  || true
+
+# Extract protobufs
 mkdir -p Protobufs ProtobufsWebui Structs
 node .github/tools/dump_javascript_protobufs.mjs
 
